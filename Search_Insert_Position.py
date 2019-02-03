c03635cd08ba488756b1c176cdc01c9440d986b7
vijayggwp <43071989+vijayggwp@users.noreply.github.com>
2019-02-02 19:28:21 -0500

    Solution

---
+++ Search_Insert_Position.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return(nums.index(target))
+        for i in range(len(nums)):
+            if(target<nums[i]):
+                return(i)
+            
+        return(len(nums))
+        
+        
