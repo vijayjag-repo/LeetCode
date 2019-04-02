842f8e65223e413f8a6b332d377a06e44182a94a
vijayjag-repo <43071989+vijayjag-repo@users.noreply.github.com>
2019-04-01 20:10:10 -0400

    Solution

--- Move_Zeroes.py
+++ Move_Zeroes.py
@@ -4,17 +4,9 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        for i in nums:
-            if(i==0):
-                count+=1
-                
-        for i in range(0,count):
+        count = collections.Counter(nums)
+        for i in range(0,count[0]):
             nums.remove(0)
-
-        i = 1
-        while(i<=count):
             nums.append(0)
-            i+=1
-            
+        
         
