b2cc549df571ee851a568f956ce4a7fb856ed3d8
vijayjag-repo <43071989+vijayjag-repo@users.noreply.github.com>
2019-04-01 21:11:00 -0400

    Updated Solution

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -2,27 +2,30 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
-        :rtype: str                   
+        :rtype: str  
+        Approach:
+        First, use zip to consider the array of strings as a matrix and find the transpose.
+        Now, run two for-loops and compare if each element in the tuple is the same.
+        If same, add it to the prefix.
+        Moreover, since the question is about prefix, we need to check if the first tuple has all same elements. 
+        If not, we can return an empty string because the first character of all the strings in the array must be the same.
+        
         """
-        new = ""
-        if(len(strs)==0):
-            return("".join(strs))
-        if(len(strs)==1):
-            return(strs[0])
-        else:
-            a = zip(*strs)
-            for i in a:
-                count = 0
-                for j in range(1,len(i)):
-                    if(a.index(i)==len(a)-1 and len(a)!=1 and new==""):
-                        return("")
-                    if(i[j-1]==i[j]):
-                        count+=1
-                    else:
-                        return(new)
-                if(count==len(i)-1):
-                    new = new + i[j]
-            return(new)
+        prefix = ""
+        value = 0
+        new = zip(*strs)
+        for i in new:
+            value+=1
+            count = 0
+            for j in range(len(i)):
+                if(i[j]==i[0]):
+                    count+=1
+                if(count==len(i)):
+                    prefix+=i[0]
+            if(value==1 and count<len(i)):
+                return("")
+      
+        return(prefix)
             
                 
                 
