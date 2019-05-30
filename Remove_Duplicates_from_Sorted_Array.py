a9026e1a7d39993485e896958f1fe94165fbcb5b
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-05-30 01:03:50 -0500

    Solution

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -1,16 +1,22 @@
-
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+        
+        We know that the input array is sorted and that we do not need to remove the duplicate elements.
+        So, we need to have the unique elements from index 0,1,2... and so on.
+        So, we initialize a variable x = 1, because, that should be the position of the next unique element.
+        Similarly, 2 will be the position of the next unique element.
+        So, whenever we encounter a new element, we just copy it to its respective index(to-be) and return the length.
         """
-        new = []
-        for i in nums:
-            if i not in new:
-                new.append(i)
+        x = 1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[x] = nums[i+1]
+                x+=1
         
-        for i in range(len(new)):
-            nums[i] = new[i]
+        return(x)
+            
         
-        return(len(new))
+        
