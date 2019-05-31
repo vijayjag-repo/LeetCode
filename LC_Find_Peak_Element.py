c5b974b51de2db7f1c77a19484ad75832c2933de
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-05-31 00:56:29 -0500

    Solution

--- LC_Find_Peak_Element.py
+++ LC_Find_Peak_Element.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if(len(nums)==1):
-            return(0)
-        if(len(nums)==2):
-            return(nums.index(max(nums)))
-        for i in range(1,len(nums)-1):
-            if(nums[i]>nums[i-1] and nums[i]>nums[i+1]):
-                return(i)
+        i = 0
+        j = len(nums)-1
+        while(i<j):
+            mid = (i+j)/2
+            if(nums[mid]>nums[mid+1]):
+                j = mid
             else:
-                return(nums.index(max(nums)))
+                i = mid + 1
         
+        return(i)
