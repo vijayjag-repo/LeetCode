b8f32e111f680afab81a4a9d8a73be7a3f3a988c
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-02 17:49:41 -0500

    Solution

--- Sort_colors.py
+++ Sort_colors.py
@@ -2,27 +2,24 @@ class Solution(object):
     def sortColors(self, nums):
         """
         :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
         count = collections.Counter(nums)
         zero = count[0]
-        one = count[1]
-        two = count[2]
-        for i in range(len(nums)):
-            if(zero>0):
+        one = zero + count[1]
+        two = one + count[2]
+        i = 0
+        while(i<len(nums)):
+            if(i<zero):
                 nums[i] = 0
-                zero-= 1
-            else:
-                if(one>0):
-                    nums[i] = 1
-                    one-= 1
-                else:
-                    if(two>0):
-                        nums[i] = 2
-                        two-= 1
-        
-        
-        
-                
-        
+                i+=1
+            elif(i<one):
+                nums[i] = 1
+                i+=1
+            elif(i<two):
+                nums[i] = 2
+                i+=1
+               
+        return(nums)
+            
             
