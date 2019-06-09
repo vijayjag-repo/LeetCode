5cc90ef1e893e14ecff0694b42519e33e13aa7c3
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-09 07:28:25 -0500

    Solution

--- LC_Majority_Element_II.py
+++ LC_Majority_Element_II.py
@@ -4,20 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        d = dict()
-        min_val = len(nums)/3
-        ans = []
-        for i in range(len(nums)):
-            if nums[i] in d:
-                d[nums[i]]+=1
+        if not nums:
+            return([])
+    
+        count_1,count_2,candidate_1,candidate_2 = 0,0,0,1
+        for i in nums:
+            if(i==candidate_1):
+                count_1+=1
+            elif(i==candidate_2):
+                count_2+=1
+            elif(count_1==0):
+                candidate_1,count_1 = i,1
+            elif(count_2==0):
+                candidate_2,count_2 = i,1
             else:
-                d[nums[i]] = 1
+                count_1-=1
+                count_2-=1
         
-        for i in d:
-            if(d[i]>min_val):
-                ans.append(i)
-        
-        return(ans)
-        
+        return [n for n in (candidate_1, candidate_2)
+                    if nums.count(n) > len(nums) // 3]
         
         
