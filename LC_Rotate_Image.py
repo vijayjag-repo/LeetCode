579718d14e7caa860abfe9679a3263676cbcb3ea
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-09 11:43:54 -0500

    Solution

--- LC_Rotate_Image.py
+++ LC_Rotate_Image.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
-        matrix[::] = zip(*matrix[::-1])
+        matrix[::] = zip(*matrix)
+        for i in range(len(nums)):
+            matrix[i] = matrix[i][::-1]
+           
