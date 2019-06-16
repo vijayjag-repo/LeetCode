4de6678412fba064da87677d90c8050db2fd9ff7
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-16 17:33:31 -0500

    Solution

---
+++ LC_Minimum_Window_Substring.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need = collections.Counter(t)            
+        missing = len(t)                         
+        start, end = 0, 0
+        i = 0
+        for j, char in enumerate(s, 1):          
+            if(need[char] > 0):
+                missing -= 1
+            need[char] -= 1
+            if(missing == 0):                    
+                while(i < j and need[s[i]] < 0):  
+                    need[s[i]] += 1
+                    i += 1
+                need[s[i]] += 1                  
+                missing+=1                     
+                if(end == 0 or j-i < end-start):  
+                    start, end = i, j
+                i += 1                           
+        return(s[start:end])
