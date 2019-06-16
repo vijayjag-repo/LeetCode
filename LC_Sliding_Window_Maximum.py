918a1e12a7d6035e07e0e45f9c5a5bc6b2c2fdad
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-16 17:33:02 -0500

    Solution

---
+++ LC_Sliding_Window_Maximum.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        q = deque()
+        result = []
+        
+        for i in range(len(nums)):
+            # the first/left (max) element is out of the current window
+            if(q and i - q[0]==k):
+                q.popleft()  
+            while(q):
+                # pop useles elements from last/right of the queue
+                if(nums[q[-1]] < nums[i]):
+                    q.pop()
+                else:
+                    break
+            q.append(i)
+            
+            if(i >= k-1): # i == k-1 is the beginning of a full window
+                result.append(nums[q[0]])
+            
+        return(result)
