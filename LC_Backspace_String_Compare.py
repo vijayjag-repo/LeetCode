b670c92f9a3f5c1f48764ebdb50187d703745fc9
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-06-24 11:40:10 -0500

    Solution

--- LC_Backspace_String_Compare.py
+++ LC_Backspace_String_Compare.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type T: str
         :rtype: bool
         """
+        # Solution 1
         a = ""
         b = ""
         
@@ -19,6 +20,37 @@ class Solution(object):
             else:
                 b = b[:-1]
         return(a==b)
+        
+        -------------------------------------------------------------------------
+        -------------------------------------------------------------------------
+        # Solution 2 using stack
+        class Solution(object):
+            def backspaceCompare(self, S, T):
+                """
+                :type S: str
+                :type T: str
+                :rtype: bool
+                """
+
+                stack_s = []
+                stack_t = []
+
+                for i in S:
+                    if(len(stack_s)>0 and i=='#'):
+                        stack_s.pop()
+                    if(i!='#'):
+                        stack_s.append(i)
+
+                for i in T:
+                    if(len(stack_t)>0 and i=='#'):
+                        stack_t.pop()
+                    if(i!='#'):
+                        stack_t.append(i)
+
+                return(stack_s==stack_t)
+                
+        
+        
         
             
         
