f1dbfdd2d3b47a111fded19ca9e3e2d44a71125e
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-14 00:27:07 -0500

    Solution

--- LC_Longest_Consecutive_Sequence.py
+++ LC_Longest_Consecutive_Sequence.py
@@ -20,3 +20,17 @@ class Solution(object):
                     longest = max(longest,current)
                     current = 1
         return(max(longest,current))
+        
+        """
+        O(n) approach. Simple idea. work with an example.
+        """
+        
+        nums = set(nums)
+        best = 0
+        for x in nums:
+            if x - 1 not in nums:
+                y = x + 1
+                while y in nums:
+                    y += 1
+                best = max(best, y - x)
+        return best
