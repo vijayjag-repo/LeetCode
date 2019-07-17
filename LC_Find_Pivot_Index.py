a9f226d231222904f5243d3b74f1a2ffa71c833d
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-17 18:24:00 -0500

    Solution

--- LC_Find_Pivot_Index.py
+++ LC_Find_Pivot_Index.py
@@ -3,6 +3,13 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
+        
+        Approach:
+        
+        First calculate the right sum.
+        Then in the next pass, remove the first element from right sum and add it to left sum. Check for equality.
+        Repeat this process.
+        
         """
         left_sum = 0
         right_sum = 0
