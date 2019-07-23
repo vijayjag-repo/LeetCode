a057684bd90c14e6e8ef93194f8dd8b681122640
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-23 16:39:51 -0500

    Solution

--- LC_Maximum_Depth_of_Binary_Tree.py
+++ LC_Maximum_Depth_of_Binary_Tree.py
@@ -1,3 +1,27 @@
+# Recursive, BFS(Queue) and Stack approach
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        
+        Recursive Solution
+        """
+        if not root:
+            return(0)
+        else:
+            left = self.maxDepth(root.left)
+            right = self.maxDepth(root.right)
+            return(1+max(left,right)
+
+# Using Queue - BFS
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -11,12 +35,49 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        if not root:
-            return(0)
-        else:
-            left = self.maxDepth(root.left)
-            right = self.maxDepth(root.right)
-            if(left>right):
-                return(left+1)
-            else:
-                return(right+1)
+        depth = 0
+        level = [root] if root else []
+        
+        while(level):
+            depth+=1
+            current = []
+            for node in level:
+                if(node.left):
+                    current.append(node.left)
+                if(node.right):
+                    current.append(node.right)
+                    
+            level = current
+        return(depth)
+                   
+# Using Stack
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        stack = []
+        depth = 0
+        stack.append((root,1))
+        
+        while(stack):
+            root,current_depth = stack.pop()
+            if(not root):
+                return(0)
+            depth = max(depth,current_depth)
+            if(root.left):
+                stack.append((root.left,current_depth+1))
+            if(root.right):
+                stack.append((root.right,current_depth+1))
+            
+        return(depth)
+
+             
