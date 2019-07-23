165eb3d6de729d4646081adc842fa1291100d1b7
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-23 11:58:17 -0500

    Solution

--- LC_Longest_Common_Prefix.py
+++ LC_Longest_Common_Prefix.py
@@ -1,3 +1,47 @@
+# Clean Solution
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        All you need is the shortest string and the longest string.
+        """
+        if(len(strs)==0):
+            return("")
+        
+        shortest = min(strs)
+        longest = max(strs)
+        
+        for index,val in enumerate(shortest):
+            if(val!=longest[index]):
+                return(shortest[:index])
+        return(shortest)
+    
+# Another approach
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        
+        Approach:
+        First get the shortest string in the list of strings.
+        Then for each character in this short string, compare it with the corresponding character in the other strings.
+        If same, keep going.
+        Else, return the string until which the character matches.
+        """
+        if(len(strs)==0):
+            return("")
+        
+        shortest = min(strs,key=len)
+        
+        for index,val in enumerate(shortest):
+            for other in strs:
+                if(other[index]!=val):
+                    return(shortest[:index])
+        return(shortest)
+                
+# Old solution using zip
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -23,7 +67,6 @@ class Solution(object):
                 prefix+=i[0]
             
         return(prefix)
-            
-                
+
                 
                 
