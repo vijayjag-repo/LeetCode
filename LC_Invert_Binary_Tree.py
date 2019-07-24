febac64d40bfd3793c1e1bc048ea27215633a41d
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-23 20:30:46 -0500

    Recursive,BFS,DFS

--- LC_Invert_Binary_Tree.py
+++ LC_Invert_Binary_Tree.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# Recursive Implementation
 class Solution(object):
     def invertTree(self, root):
         """
@@ -20,4 +20,49 @@ class Solution(object):
         
         self.invertTree(root.left)
         self.invertTree(root.right)
+        return(root)
+    
+# BFS - Queue
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        queue = collections.deque([(root)])
+        while(queue):
+            node = queue.popleft()
+            if node:
+                node.left,node.right = node.right,node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        return(root)
+    
+# DFS - Stack
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        stack = [root]
+        while(stack):
+            node = stack.pop()
+            if node:
+                node.left,node.right = node.right,node.left
+                stack.extend([node.left,node.right])
         return(root)
