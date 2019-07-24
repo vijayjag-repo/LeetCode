e0baed6ae69fecade3e3f25e238a1539d37bb079
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-24 16:14:59 -0500

    Recursive, BFS and DFS

--- LC_Same_Tree.py
+++ LC_Same_Tree.py
@@ -1,10 +1,13 @@
+# Recursive, DFS, BFS
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# Approach:
+# All approaches are straightforward.
+# Notice the use of pop() in DFS and BFS. 
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -20,5 +23,61 @@ class Solution(object):
             else:
                 return(False)
         
-        
+# DFS Stack
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        stack = [(p,q)]
+        while(stack):
+            node1,node2 = stack.pop()
+            if(not node1 and not node2):
+                continue
+            elif None in [node1,node2]:
+                return(False)
+            elif(node1.val!=node2.val):
+                return(False)
+            else:
+                stack.append((node1.left,node2.left))
+                stack.append((node1.right,node2.right))
+        return(True)
+    
+# BFS Queue
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        stack = [(p,q)]
+        while(stack):
+            node1,node2 = stack.pop(0)
+            if(not node1 and not node2):
+                continue
+            elif None in [node1,node2]:
+                return(False)
+            elif(node1.val!=node2.val):
+                return(False)
+            else:
+                stack.append((node1.left,node2.left))
+                stack.append((node1.right,node2.right))
+        return(True)
             
