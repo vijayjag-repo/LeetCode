945826f2e02654602b9e28d01800355f85a11067
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-26 17:10:50 -0500

    Heap added

--- LC_Merge_K_Sorted_Lists.py
+++ LC_Merge_K_Sorted_Lists.py
@@ -1,4 +1,5 @@
 from Queue import *
+import heapq
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -30,3 +31,30 @@ class Solution(object):
             if curr.next: 
                 q.put((curr.next.val, curr.next))
         return dummy.next
+    
+# Using heap
+import heapq
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        curr = dummy = ListNode(-1)
+        h = [(node.val,node) for node in lists if node]
+        heapq.heapify(h)
+        while(h):
+            value,node = h[0]
+            if(not node.next):
+                heapq.heappop(h)
+            else:
+                heapq.heapreplace(h,(node.next.val,node.next))
+            curr.next = node
+            curr = curr.next
+        return(dummy.next)
