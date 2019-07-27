3e81af0acd490715ece4ee7330e028866167b23a
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-26 20:14:34 -0500

    sort,heap

--- LC_Meeting_Rooms.py
+++ LC_Meeting_Rooms.py
@@ -31,4 +31,17 @@ class Solution(object):
                 return(False)
         return(True)
            
-                
+# Normal Sort and compare
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        if not intervals:
+            return(True)
+        intervals.sort()
+        for i in range(1,len(intervals)):
+            if(intervals[i][0]<intervals[i-1][1]):
+                return(False)
+        return(True)
