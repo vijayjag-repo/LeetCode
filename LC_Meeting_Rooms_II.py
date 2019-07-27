53b9460d42d63fc957c85cca23813113e796b21d
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-26 22:19:24 -0500

    Solution

--- LC_Meeting_Rooms_II.py
+++ LC_Meeting_Rooms_II.py
@@ -19,15 +19,12 @@ class Solution(object):
         """
         if not intervals:
             return(0)
-        
-        free = []
-        intervals.sort(key = lambda x : x.start)
-        heapq.heappush(free,intervals[0].end)
-        
-        for i in intervals[1:]:
-            if(free[0]<=i.start):
-                heapq.heappop(free)
-            heapq.heappush(free,i.end)
-            
-        return(len(free))
+        intervals.sort()
+        rooms = []
+        heapq.heappush(rooms,intervals[0][1])
+        for i in range(1,len(intervals)):
+            if(rooms[0]<=intervals[i][0]):
+                heapq.heappop(rooms)
+            heapq.heappush(rooms,intervals[i][1])
+        return(len(rooms))
     
