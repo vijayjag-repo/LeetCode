5bb3621ac3d204b88e90e48139a730fe178df4e8
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-07-30 17:42:00 -0500

    Solution

--- LC_Sort_colors.py
+++ LC_Sort_colors.py
@@ -1,3 +1,26 @@
+# O(n) and O(1) space solution
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        white = red = 0
+        blue = len(nums)-1
+        
+        while(white<=blue):
+            if(nums[white]==0):
+                nums[white],nums[red] = nums[red],nums[white]
+                white+=1
+                red+=1
+            elif(nums[white]==1):
+                white+=1
+            else:
+                nums[white],nums[blue] = nums[blue],nums[white]
+                blue-=1
+                
+# naive approach: just count the red,white,blue and overwrite 
+
 class Solution(object):
     def sortColors(self, nums):
         """
