6dbd6a79123b730bb277d945ac980c31aae44fa0
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-08-04 11:50:38 -0500

    Solution

---
+++ LC_Longest_Palindromic_Substring.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = ""
+        def helper(s,beg,end):
+            while(beg>=0 and end<len(s) and s[beg]==s[end]):
+                beg-=1
+                end+=1
+            return(s[beg+1:end])
+            
+        for i in range(len(s)):
+            temp = helper(s,i,i)
+            ans = max(ans,temp,key=len)
+            
+            temp = helper(s,i,i+1)
+            ans = max(ans,temp,key=len)
+        
+        return(ans)
+        
+        
