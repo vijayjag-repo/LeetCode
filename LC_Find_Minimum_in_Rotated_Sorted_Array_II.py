c94d2c15ff338874c47bd6787a1844cd28216246
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-08-05 12:41:26 -0500

    Solution

---
+++ LC_Find_Minimum_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
+        while low < high:
+            mid = (low + high)/2
+            if nums[mid] > nums[high]:
+                low = mid + 1
+            elif nums[mid] < nums[high]:
+                high = mid
+            else:
+                high = high - 1
+        return nums[low]
