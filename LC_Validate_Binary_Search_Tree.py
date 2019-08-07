b7fc3502da746a2f6ecf79cf059d54d68100d7d1
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-08-07 12:31:02 -0500

    Recursive, Iterative

--- LC_Validate_Binary_Search_Tree.py
+++ LC_Validate_Binary_Search_Tree.py
@@ -1,3 +1,4 @@
+# Iterative method with less space
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -10,6 +11,69 @@ class Solution(object):
         """
         :type root: TreeNode
         :rtype: bool
+        Approach:
+        
+        First you store only the left nodes. the leftmost node will have the least value in the BST.
+        """
+        stack = []
+        inorder = float('-inf')
+        
+        while(stack or root):
+            while(root):
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if(root.val<=inorder):
+                return(False)
+            inorder = root.val
+            root = root.right
+        return(True)
+    
+# Recursive method
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(node,lower=float('-inf'),upper=float('inf')):
+            if not node:
+                return(True)
+            
+            val = node.val
+            if(val<=lower or val>=upper):
+                return(False)
+            
+            if not helper(node.right,val,upper):
+                return(False)
+            if not helper(node.left,lower,val):
+                return(False)
+            return(True)
+        return(helper(root))
+    
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        
+        Approach:
+        
+        This approach makes use of inorder traversal and goes through the tree entirely.
         """
         new = []
         def helper(root):
