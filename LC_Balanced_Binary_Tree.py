93e034fe460c99659631c6cd7ed0b8cddb5fb6f2
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-09-30 18:40:34 -0400

    Solution

--- LC_Balanced_Binary_Tree.py
+++ LC_Balanced_Binary_Tree.py
@@ -29,3 +29,37 @@ class Solution(object):
         
         helper(root)
         return(False if self.max_difference>1 else True)
+    
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    
+    def depth(self,root):
+        if not root:
+            return(0)
+        else:
+            left = self.depth(root.left)
+            right = self.depth(root.right)
+            if(left==-1 or right==-1):
+                return(-1)
+            if(abs(left-right)>1):
+                return(-1)
+            return(1+max(left,right))
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        
+        Approach:
+        
+        Keep computing depths as long as it is valid. If it is not valid, return -1 or False appropriately.
+        This -1 or False is propagated to the parent/root node and finally you can return False/True based on this.
+        """
+        val = self.depth(root)
+        return(False if val==-1 else True)
+    
