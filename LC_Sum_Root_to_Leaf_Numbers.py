c3a607d6b8a0b0f1ea06ade86cf61e1d1353f135
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-10-01 20:15:56 -0400

    Recursive, DFS

--- LC_Sum_Root_to_Leaf_Numbers.py
+++ LC_Sum_Root_to_Leaf_Numbers.py
@@ -5,6 +5,39 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# Recursive solution 
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return(0)
+        self.total = 0
+        def helper(root,sums):
+            if not root:
+                return
+            if not root.left and not root.right:
+                sums+=root.val
+                self.total+=sums
+                return
+            
+            sums+=root.val
+            if(root.left):
+                helper(root.left,sums*10)
+            if(root.right):
+                helper(root.right,sums*10)
+        
+        helper(root,0)
+        return(self.total)
+                
 class Solution(object):
     def sumNumbers(self, root):
         """
