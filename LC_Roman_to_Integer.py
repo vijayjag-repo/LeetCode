512fd00bbcb6fb0afbb7ef6189fa65deafe488bb
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-10-06 14:30:06 -0400

    Solution

--- LC_Roman_to_Integer.py
+++ LC_Roman_to_Integer.py
@@ -3,6 +3,32 @@ class Solution(object):
         """
         :type s: str
         :rtype: int
+        
+        Approach:
+        
+        If lesser than next, subtract else add.
+        Simple approach.
+        """
+        if not s:
+            return 0
+        
+        d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        ans = 0
+        
+        for i in range(len(s)-1):
+            if(d[s[i]]<d[s[i+1]]):
+                ans-=d[s[i]]
+            else:
+                ans+=d[s[i]]
+            
+        return ans+d[s[-1]]
+        
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
         """
         sums = 0
         d = dict()
