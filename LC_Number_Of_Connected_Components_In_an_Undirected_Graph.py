aff1dbe5f85befc9c30d4071891a3aa92d138a58
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-10-09 19:11:32 -0400

    Union Find

--- LC_Number_Of_Connected_Components_In_an_Undirected_Graph.py
+++ LC_Number_Of_Connected_Components_In_an_Undirected_Graph.py
@@ -4,6 +4,35 @@ class Solution(object):
         :type n: int
         :type edges: List[List[int]]
         :rtype: int
+        Approach:
+        
+        Union-find using rank. Nice solution
+        """
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+            
+        def union(xy):
+            x, y = map(find, xy)
+            if rank[x] < rank[y]:
+                parent[x] = y
+            else:
+                parent[y] = x
+                if rank[x] == rank[y]:
+                    rank[x] += 1
+        
+        parent, rank = range(n), [0] * n
+        map(union, edges)
+        return len({find(x) for x in parent})
+
+
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
         
         Approach:
         
