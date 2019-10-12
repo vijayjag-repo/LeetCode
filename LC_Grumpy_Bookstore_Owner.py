5d16dcd90d2e1f479f7daf308fb334b084c6dabe
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-10-12 18:27:10 -0400

    Solution

--- LC_Grumpy_Bookstore_Owner.py
+++ LC_Grumpy_Bookstore_Owner.py
@@ -5,32 +5,26 @@ class Solution(object):
         :type grumpy: List[int]
         :type X: int
         :rtype: int
-        
-        Approach:
-        1) First, you count the satisfied customers and set satisifed customers index to 0.
-        2) Now, you just have to count the max sum of window of length X.
+ 
         """
+        minval = [customers[i] for i in range(len(customers)) if grumpy[i]==0]
+        maxval = 0
+        temp = 0
 
-        i = 0
-        base = 0
-        sums = 0
-        prev_sums = 0
-        
-        for i in range(len(grumpy)):
-            if(grumpy[i]==0):
-                base+=customers[i]
-                customers[i]=0
-        
-        i = 0
         for i in range(len(customers)):
             if(i<X):
-                prev_sums+=customers[i]
-                sums = prev_sums
+                if(grumpy[i]==1):
+                    temp+=customers[i]
             else:
-                prev_sums = prev_sums + customers[i] - customers[i-X]
-                sums = max(sums,prev_sums)
-                
-        return(sums+base)
+                if(grumpy[i]==1):
+                    temp+=customers[i]
+                if(grumpy[i-X]==1):
+                    temp-=customers[i-X]
+                    
+            maxval = max(maxval,temp)
+        
+        return sum(minval)+maxval
             
                 
+        
         
