3c75bddb13e4950fd79f11f2e223c5d98b8668e6
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-10-14 19:12:10 -0400

    Solution

--- LC_Valid_Sudoku.py
+++ LC_Valid_Sudoku.py
@@ -4,30 +4,33 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         
-        LeetCode Discussion Solution
+        Approach:
+        
+        First check for the row validity and then check for column validity and then check for square validity.
         """
-        return(self.is_row_valid(board) and self.is_col_valid(board) and self.is_square_valid(board))
-
-    def is_row_valid(self, board):
-        for row in board:
-            if not self.is_unit_valid(row):
-                return(False)
-        return(True)
-
-    def is_col_valid(self, board):
-        for col in zip(*board):
-            if not self.is_unit_valid(col):
-                return(False)
-        return(True)
-
-    def is_square_valid(self, board):
-        for i in (0, 3, 6):
-            for j in (0, 3, 6):
-                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not self.is_unit_valid(square):
-                    return(False)
-        return(True)
-
-    def is_unit_valid(self, unit):
-        unit = [i for i in unit if i != '.']
-        return(len(set(unit)) == len(unit))
+        def row_valid(board):
+            for row in board:
+                if not unit_valid(row):
+                    return False
+            return True
+        
+        def col_valid(board):
+            for col in zip(*board):
+                if not unit_valid(col):
+                    return False
+            return True
+        
+        def square_valid(board):
+            for i in (0,3,6):
+                for j in (0,3,6):
+                    square = [board[x][y] for x in range(i,i+3) for y in range(j,j+3)]
+                    if not unit_valid(square):
+                        return False
+            return True
+            
+        def unit_valid(unit):
+            val = [i for i in unit if i!='.']
+            return len(set(val))==len(val)
+            
+        return row_valid(board) and col_valid(board) and square_valid(board)
+        
