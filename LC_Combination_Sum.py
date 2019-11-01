ec6027cc7a9fdcc7ee8b5b8d5a8984d8fb982a9a
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-11-01 15:01:53 -0400

    Solution

--- LC_Combination_Sum.py
+++ LC_Combination_Sum.py
@@ -4,6 +4,13 @@ class Solution(object):
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
+        
+        Approach:
+        
+        Standard Backtracking. 
+        Key idea is that, we can use the same number multiple times. Inside the for loop while calling dfs(),
+        we pass the index as i instead of i+1. i -> gaurantees that the same element is used multiple times whereas i+1 will ensure that,
+        only the unique elements add up to a particular sum.
         """
         res = []
         candidates.sort()
