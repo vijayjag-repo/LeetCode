fcf5e417b14463fe4089aa6382375f1080f7cf13
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-11-03 14:37:08 -0500

    Tarjans, DFS

--- LC_Critical_Connections_in_a_Network.py
+++ LC_Critical_Connections_in_a_Network.py
@@ -5,6 +5,45 @@ class Solution(object):
         :type connections: List[List[int]]
         :rtype: List[List[int]]
         
+        Approach:
+        
+        Tarjans - LeetCode Discussion
+        """
+        graph = [[] for _ in range(n)]
+        
+        for (x,y) in connections:
+            graph[x].append(y)
+            graph[y].append(x)
+        
+        N = len(connections)
+        lev = [None] * N
+        low = [None] * N
+        
+        def dfs(node,parent,level):
+            if lev[node] is not None:
+                return
+            lev[node] = low[node] = level
+            for nei in graph[node]:
+                if not lev[nei]:
+                    dfs(nei,node,level+1)
+            cur = min([level]+[low[nei] for nei in graph[node] if nei!=parent])
+            low[node] = cur
+        
+        dfs(0,None,0)
+        ans = []
+        
+        for (u,v) in connections:
+            if low[u]>lev[v] or low[v]>lev[u]:
+                ans.append([u,v])
+        return ans
+              
+class Solution(object):
+    def criticalConnections(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: List[List[int]]
+        
         TLE for 4 testcases
         """
         graph = [[] for _ in range(n)]
