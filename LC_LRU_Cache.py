50138d36d33b4eb48e87e13b4432cef11f3687ae
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-11-07 18:52:26 -0500

    OrderedDict, Hashmap+DoublyLinkedList

--- LC_LRU_Cache.py
+++ LC_LRU_Cache.py
@@ -51,3 +51,46 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+import collections
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.d = collections.OrderedDict()
+        self.capacity = capacity
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.d:
+            return -1
+        else:
+            val = self.d.pop(key)
+            self.d[key] = val
+            return self.d[key]
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.d:
+            self.d.pop(key)
+        
+        if(len(self.d)>=self.capacity):
+            self.d.popitem(last=False)
+        self.d[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
