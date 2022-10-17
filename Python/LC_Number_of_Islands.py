b0846959afe2d038f72139e5e6b995cb6e2b977a
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2022-10-17 20:29:45 +0530

    Update LC_Number_Of_Islands.py
    Little deep dive again :)

--- Python/LC_Number_of_Islands.py
+++ Python/LC_Number_of_Islands.py
@@ -1,67 +1,85 @@
+# DFS approach
 class Solution(object):
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
-        Approach:
-        DFS
-        First, we search for '1' in the grid. 
-        Second, we call a method to change this '1' to '0. Also, we check for the neigboring '1's(only vertical and
-        horizontal neighbors) and set their value to '0'(connected components form an island).
-        Third, there is an if condition to return 0, incase we go outside the grid or the grid value is '0'.
-        For example, if grid[0][0]=='1' and we check the upper horizontal neighbor which doesn't exist, then it means that
-        it's value is '0'(water)
-        """
-        if(grid==None):
-            return(0)
-        count = 0
-        def dfs(i,j):
-            if(i<0 or i>=len(grid) or j<0 or j>=len(grid[i]) or grid[i][j]=='0'):
-                return(0)
-            else:
-                grid[i][j] = '0'
-                dfs(i-1,j)
-                dfs(i+1,j)
-                dfs(i,j-1)
-                dfs(i,j+1)
-                return(1)
 
-        count = [dfs(i,j) for i in range(len(grid)) for j in range(len(grid[i])) if grid[i][j]=='1']
-        return(sum(count))
+        Intuition: If we find land (1 == land and 0 == water), then we search all its neighbors to find land as well.
+        If there's no land and only water, we've found our exit condition to add one island to our count.
+
+        1. Search for '1' in the grid.
+        2. From the current position, call all 4 neighbors (vertical and horizontal) to find if they are '1' or not(ie: land or not)
+        3. Since we're employing DFS here, this traversal will continue as long as we continue to find '1'.
+        4. One key idea to remember - we should change the value of the land that we've already visited to '0'.
+            We need to do this because, we could get stuck in an infinite loop.
+        5. Additionally, we also need to ensure that a given cell is land (index within the grid and not equal to water)
+        6. Once the DFS call ends, that means we've found an island. Increment the count
+        7. Now, we have successfully turned an island of '1's into '0's. Repeat step 1 for the remaining '1's and finally return count.
+
+        Time complexity: O(m * n)
+        Space complexity: O(m * n)
+        """
+        no_of_islands = 0
+
+        def valid_cell(row, col):
+            if (row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0'):
+                return False
+            return True
+
+        def dfs(row, col):
+            if (not valid_cell(row, col)):
+                return 0
+            else:
+                grid[row][col] = '0'
+                dfs(row + 1, col)
+                dfs(row - 1, col)
+                dfs(row, col + 1)
+                dfs(row, col - 1)
+                return 1
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (grid[i][j] == '1'):
+                    no_of_islands += dfs(i, j)
+
+        return no_of_islands
+
 # BFS Approach
 class Solution(object):
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
-        """
-        if not grid:
-            return(0)
-        def valid(i,j):
-            if(i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=='0'):
-                return(False)
-            return(True)
-        
-        def bfs(i,j):
-            queue = collections.deque()
-            queue.append((i,j))
-            directions = [(0,-1),(-1,0),(0,1),(1,0)]
-            grid[i][j] = '0'
-            while(queue):
-                x,y = queue.popleft()
-                for d in directions:
-                    nx,ny = x + d[0],y + d[1]
-                    if(valid(nx,ny)):
-                        queue.append((nx,ny))
-                        grid[nx][ny]='0'
-            return(1)
-                                
-        ans=[bfs(i,j) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j]=='1']
-        return(sum(ans))
-            
 
-        
-    
-            
-            
-                
+        Same as DFS approach but the only variation is that,
+        while looking at a given '1', we only traverse the immediate 4 neighbors of this cell and not their neighbors and so on.
+        And to do that, we'll be using a deque instead of a stack to pop elements from the left.
+
+        Time complexity: O(m * n)
+        Space complexity: O(min(m,n))
+        """
+        no_of_islands = 0
+
+        def valid_cell(row, col):
+            if (row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0'):
+                return False
+            return True
+
+        def bfs(row, col):
+            q = collections.deque([(row, col)])
+            grid[row][col] = '0'
+            while q:
+                row, col = q.popleft()
+                for (x, y) in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                    if (valid_cell(x, y)):
+                        q.append((x, y))
+                        grid[x][y] = '0'
+            return 1
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (grid[i][j] == '1'):
+                    no_of_islands += bfs(i, j)
+
+        return no_of_islands
